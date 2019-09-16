fd20ea4ef4310ef025747641067fe7282ed2dd6b
Daniel Flook <daniel@flook.org>
2019-09-16 19:56:55 +0100

    :wrench: Update test images

--- .circleci/build-images.sh
+++ .circleci/build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DATE=$(date -I)
-docker build -t danielflook/python-minifier-build:fedora30-$DATE -f Dockerfile-fedora30 .
-docker build -t danielflook/python-minifier-build:fedora28-$DATE -f Dockerfile-fedora28 .
+docker build -t danielflook/python-minifier-build:fedora30-$DATE -f Dockerfile-fedora30 . --no-cache
+docker build -t danielflook/python-minifier-build:fedora28-$DATE -f Dockerfile-fedora28 . --no-cache
