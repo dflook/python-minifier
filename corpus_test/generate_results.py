c9eb45274fffa25daf1395bfbf6457310787cb8f
Daniel Flook <daniel@flook.org>
2023-04-09 22:13:16 +0100

    Trigger test corpus workflow from xtest

--- corpus_test/generate_results.py
+++ corpus_test/generate_results.py
@@ -7,9 +7,6 @@ import time
 import python_minifier
 from result import Result, ResultWriter
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
 try:
     RE = RecursionError
 except NameError:
@@ -56,16 +53,21 @@ def minify_test(corpus_path, corpus_entry):
     return result
 
 
-def corpus_test(corpus_path, results_path, sha):
+def corpus_test(corpus_path, results_path, sha, regenerate_results):
     """
     :param str corpus_path: Path to the corpus
     :param str results_path: Path to the results directory
     :param str sha: The python-minifier sha we are testing
+    :param bool regenerate_results: Regenerate results even if they are present
     """
     corpus_entries = os.listdir(corpus_path)
 
     python_version = '.'.join([str(s) for s in sys.version_info[:2]])
     results_file_path = os.path.join(results_path, 'results_' + python_version + '_' + sha + '.csv')
+
+    if os.path.isfile(results_file_path) and not regenerate_results:
+        print('Results file already exists: %s', results_file_path)
+        return
 
     with ResultWriter(results_file_path) as result_writer:
         for entry in corpus_entries:
@@ -73,15 +75,18 @@ def corpus_test(corpus_path, results_path, sha):
             result = minify_test(corpus_path, entry)
             result_writer.write(result)
 
+def bool_parse(value):
+    return value == 'true'
 
 def main():
     parser = argparse.ArgumentParser(description='Test python-minifier on a corpus of Python files.')
     parser.add_argument('corpus_dir', type=str, help='Path to corpus directory', default='corpus')
     parser.add_argument('results_dir', type=str, help='Path to results directory', default='results')
     parser.add_argument('minifier_sha', type=str, help='The python-minifier sha we are testing')
+    parser.add_argument('regenerate_results', type=bool_parse, help='Regenerate results even if they are present', default='false')
     args = parser.parse_args()
 
-    corpus_test(args.corpus_dir, args.results_dir, args.minifier_sha)
+    corpus_test(args.corpus_dir, args.results_dir, args.minifier_sha, args.regenerate_results)
 
 
 if __name__ == '__main__':
