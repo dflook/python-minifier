326b0f3c8220fd4314a14eb4891c3e58a284c1de
Daniel Flook <daniel@flook.org>
2024-08-06 18:04:55 +0100

    f

--- ascii.py
+++
@@ -1,9 +0,0 @@
-from python_minifier import minify
-
-with open('snippet.py', 'rb') as f:
-    source = f.read()
-
-minified = minify(source)
-
-with open('minified.py', 'w', encoding='ascii', errors='backslashreplace') as f:
-    f.write(minified)
